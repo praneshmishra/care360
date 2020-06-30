1a69cbc192a0b38f839b396f410117cbc4166228
Veer Singh <vishayvin@gmail.com>
2020-06-30 16:02:26 +0530

    For medication card in home page adding medication schedule

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $("#diagnostics").click(function(){
         $("main").load("diagnostics.html");
     });
+
+    $(".showCardContent").click(function(){
+        $(this).addClass("mb-0");
+        $(this).next(".cardContent").slideToggle();
+    })
 });
