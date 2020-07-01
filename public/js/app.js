35f288e272e6dbfd914d78c4dbcab8ad0f4e0a7e
saarika06 <saarika06@gmail.com>
2020-07-01 20:45:36 +0530

    breadcrumbs changes

--- public/js/app.js
+++ public/js/app.js
@@ -5,34 +5,33 @@ $(document).ready(function(){
     $(document).on('click','.link-diagnostic',function(){
         $("main").load("/html/diagnostics.html");
         $("#breadcrumbs").show();
-        if (!$(".diagnostics").length) {
-            $(".survey").remove();
-            $(".medication").remove();
-            $("#home").after("<li class='breadcrumb-item active diagnostics'>Diagnostics</li>"); 
-        }
+        $(".link-bc-last").hide();
+        $(".link-bc-secondary").show();
+        $(".link-bc-secondary").text("Diagnostics");
+        $(".link-bc-secondary").addClass("active");
     });
 
     $(document).on('click','.link-survey',function(){
         $("main").load("/html/survey.html");
         $("#breadcrumbs").show();
-        if (!$(".survey").length) {
-            $(".diagnostics").remove();
-            $(".medication").remove();
-            $("#home").after("<li class='breadcrumb-item active survey'>Survey</li>"); 
-        }
+        $(".link-bc-last").hide();
+        $(".link-bc-secondary").show();
+        $(".link-bc-secondary").text("Survey");
+        $(".link-bc-secondary").addClass("active");
     });
 
     $(document).on('click','.medication-schedule',function(){
         $("main").load("/html/medication-schedule.html");
         $("#breadcrumbs").show();
-        if (!$(".medication").length) {
-            $(".survey").remove();
-            $(".diagnostics").remove();
-            $("#home").after("<li class='breadcrumb-item medication'>Medication</li><li class='breadcrumb-item active medication'>Schedule</li>"); 
-        }
+        $(".link-bc-secondary").show();
+        $(".link-bc-secondary").html('<a href="javascript:void(0)" class="link-medication">Medication</a>');
+        $(".link-bc-secondary").removeClass("active");
+        $(".link-bc-last").show();
+        $(".link-bc-last").text("Schedule");
+        $(".link-bc-last").addClass("active");
     });
 
-    $(document).on('click','.link-home',function(){
+    $(document).on('click','.link-bc-home',function(){
         $("#breadcrumbs").hide();
         $("main").load("/html/cards.html");
     });
