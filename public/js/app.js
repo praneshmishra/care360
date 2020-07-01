05feb99fad46c941fa71e902f74bcd32ffc2875c
saarika06 <saarika06@gmail.com>
2020-07-01 10:01:54 +0530

    linking survey and home page

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,27 @@ $(document).ready(function(){
     $("main").load("/html/cards.html");
     $("#breadcrumbs").hide();
 
-    $(document).on('click','#diagnostics',function(){
+    $(document).on('click','.link-diagnostic',function(){
         $("main").load("/html/diagnostics.html");
         $("#breadcrumbs").show();
-        $("#home").after("<li class='breadcrumb-item active'>Diagnostics</li>");      
+        if (!$(".diagnostics").length) {
+            $(".survey").remove();
+            $("#home").after("<li class='breadcrumb-item active diagnostics'>Diagnostics</li>"); 
+        }
     });
 
-    $(document).on('click','#home',function(){
+    $(document).on('click','.link-survey',function(){
+        $("main").load("/html/survey.html");
+        $("#breadcrumbs").show();
+        if (!$(".survey").length) {
+            $(".diagnostics").remove();
+            $("#home").after("<li class='breadcrumb-item active survey'>Survey</li>"); 
+        }
+    });
+
+    $(document).on('click','.link-home',function(){
         $("#breadcrumbs").hide();
         $("main").load("/html/cards.html");
-        $("#breadcrumbs").hide();
     });
 
 });
