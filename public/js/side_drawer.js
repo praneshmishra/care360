7255d9529f02034160953156808b9b5b1201be06
abid08 <tajul.abideen@gmail.com>
2020-07-01 10:25:50 +0530

    navbar menu JS changes for dropdown

--- public/js/side_drawer.js
+++ public/js/side_drawer.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $('button.navbar-toggler').on('click', function () {
         openSideDrawer();
     });
-    $('#links, #other-links, #side-drawer-void').on('click', function () {
-        closeSideDrawer();
+    $('#links a, #other-links a, #side-drawer-void, button.medication-btn').on('click', function () {
+        closeSideDrawer($(this), event);
     });
     /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
     var dropdown = document.getElementsByClassName("dropdown-btn");
@@ -31,8 +31,13 @@ function openSideDrawer() {
     document.getElementById("side-drawer-void").classList.remove("d-none");
 }
 
-function closeSideDrawer() {
-    document.getElementById("side-drawer").style.left = "-336px";
-    document.getElementById("side-drawer-void").classList.add("d-none");
-    document.getElementById("side-drawer-void").classList.remove("d-block");
+function closeSideDrawer(thisObj, e) {
+    if (thisObj.attr('class').indexOf('medication-btn') !== -1) {
+        e.stopPropagation();
+    }
+    if (thisObj.attr('class').indexOf('medication-btn') === -1) {
+        document.getElementById("side-drawer").style.left = "-336px";
+        document.getElementById("side-drawer-void").classList.add("d-none");
+        document.getElementById("side-drawer-void").classList.remove("d-block");
+    }
 }
