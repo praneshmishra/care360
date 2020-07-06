ec48a8c066574abfa314a3cc735edce9a2572bf8
saarika06 <saarika06@gmail.com>
2020-07-06 19:15:13 +0530

    diagnostic view calendar and table changes

--- public/js/diagnostics-view.js
+++ public/js/diagnostics-view.js
@@ -1,22 +1,34 @@
-$(document).on('click','#toggleView input', function() {
-    
+$(document).ready(function() {
+    $('#gridTable').DataTable({
+        "paging":   false,
+        "info":     false,
+        "searching": false
+    });
+    $('#graphTable').DataTable({
+        "paging":   false,
+        "info":     false,
+        "searching": false
+    });
+});
+
+$(document).on('click','#toggleView input', function() {    
     if($(this).data().val === "grid") {      
         $("#toggleView .btn-group label.v-grid").addClass("active");
         $("#toggleView .btn-group label.v-graph").removeClass("active");
         $("#toggleView .btn-group label.v-grid input").attr("checked");
         $("#toggleView .btn-group label.v-graph input").removeAttr("checked");
-        $("#graphContent").fadeOut("fast");
-        $("#gridContent").fadeIn("fast");
+        $("#graphContent").hide();
+        $("#gridContent").show();
     }
 
     if($(this).data().val === "graph") {
         $("#toggleView .btn-group label.v-graph input").attr("checked");
         $("#toggleView .btn-group label.v-grid input").removeAttr("checked");
-        $("#gridContent").fadeOut("fast");
-        $("#graphContent").fadeIn("fast");
+        $("#gridContent").hide();
+        $("#graphContent").show();
         $("#toggleView .btn-group label.v-graph").addClass("active");
         $("#toggleView .btn-group label.v-grid").removeClass("active");
     }
 });
 
-//$("#datepicker").datepicker();
+$("#datepicker").datepicker();
