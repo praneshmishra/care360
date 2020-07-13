3c1305dcd8c538fdb8d183894c397d256c783fdb
saarika06 <saarika06@gmail.com>
2020-07-13 17:41:27 +0530

    medication delete changes

--- public/js/medication-schedule.js
+++ public/js/medication-schedule.js
@@ -2,11 +2,7 @@ window.location.href.indexOf("medication-schedule") > -1 ? $('#calendarCarousel'
     interval: false
 }) : false;
 
-var medicationData;
-
 $(document).ready(function () {
-    
-    showMedicationList();
 
     $(document).on('click', '.notify', function () {
         $(this).toggleClass("active");
@@ -63,29 +59,5 @@ $(document).ready(function () {
             $("#scheduleWrap").fadeIn("fast");
         }
     });
-
-    $(document).on('click', '#medicationList', function () {
-        showMedicationList();
-    });
-
-    function showMedicationList() {
-        $.ajax({
-            type: 'GET',
-            dataType: 'json',
-            url: 'json/medication.json',
-            success: function (data) {
-                var medicationlist = "";
-                medicationData = data;
-                $.each(data.medication, function (key, value) {
-                    medicationlist = medicationlist + '<div class="card-body"><div class="row"><div class="col-1 col-sm-2 icon"><span>&#9737;</span></div>' +
-                        '<div class="col-7 col-sm-6 info"><h6>' + value.name + ' ' + value.dose + '</h6>' +
-                        '<p>' + value.frequency + ', ' + value.suggestedTime + '</p></div>' +
-                        '<div class="col-4 col-sm-4 action"><div class="align-center">' +
-                        '<span class="delete"></span><span class="edit"></span></div></div></div></div>';
-                });
-                $('#collapseOneamo').html(medicationlist);
-            }
-        });
-    }
 
 });
--- public/js/medication.js
+++ public/js/medication.js
@@ -1,7 +1,11 @@
 var medicationData;
 
-$.getJSON("json/medication.json", function (data) {
-    medicationData = data;
+$(document).ready(function () {
+    showMedicationList();
+});
+
+$(document).on('click', '#medicationList', function () {
+    showMedicationList();
 });
 
 $(document).on('click', '#addMedication', function () {
@@ -18,19 +22,10 @@ $(document).on('click', '#addMedication', function () {
         "notes": $("medicationNotes").val()
     };
     newData.medication.push(toDo);
-    $.ajax({
-        type: 'POST',
-        data: JSON.stringify(newData),
-        contentType: 'application/json',
-        url: '/add-tasks',
-        success: function (data) {
-            console.log('success');
-            console.log(JSON.stringify(newData));
-        }
-    });
-    $("#medicationName option[value='default']").attr("selected",true);
-    $("#medicationDose option[value='default']").attr("selected",true);
-    $("#medicationFrequency option[value='default']").attr("selected",true);
+    updateJson(newData);
+    $("#medicationName option[value='default']").attr("selected", true);
+    $("#medicationDose option[value='default']").attr("selected", true);
+    $("#medicationFrequency option[value='default']").attr("selected", true);
     $("#medicationTime").val("");
     $("#medicationStartDate").val("");
     $("#medicationEndDate").val("");
@@ -39,3 +34,52 @@ $(document).on('click', '#addMedication', function () {
     $("#collapseTwoamo").removeClass("show");
 
 });
+
+$(document).on('click', '.medication-delete', function () {
+    var currentMedication = $(this).parents(".card-body").data("medication");
+    var medicationArray = medicationData;
+    //var removeMedicationArray = medicationArray.medication;
+    //  removeMedicationArray.medication.removeValue('name', currentMedication);
+    jQuery.each(medicationArray.medication, function (i, val) {
+        if (val.name === currentMedication) // delete index
+        {
+            medicationArray.medication.splice(i, 1);
+            return false;
+        }
+    });
+    updateJson(medicationArray);
+    showMedicationList();
+});
+
+function updateJson(updatedData) {
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(updatedData),
+        contentType: 'application/json',
+        url: '/add-tasks',
+        success: function (data) {
+            console.log('success');
+            console.log(JSON.stringify(newData));
+        }
+    });
+}
+
+function showMedicationList() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'json/medication.json',
+        success: function (data) {
+            var medicationlist = "";
+            medicationData = data;
+            $.each(data.medication, function (key, value) {
+                medicationlist = medicationlist + '<div class="card-body" data-medication="' + value.name + '"><div class="row"><div class="col-1 col-sm-2 icon"><span>&#9737;</span></div>' +
+                    '<div class="col-7 col-sm-6 info"><h6>' + value.name + ' ' + value.dose + '</h6>' +
+                    '<p>' + value.frequency + ', ' + value.suggestedTime + '</p></div>' +
+                    '<div class="col-4 col-sm-4 action"><div class="align-center">' +
+                    '<span class="medication-delete"></span><span class="edit"></span></div></div></div></div>';
+            });
+            $('#collapseOneamo').html(medicationlist);
+        }
+    });
+}
